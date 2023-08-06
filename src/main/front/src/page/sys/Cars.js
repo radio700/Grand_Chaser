f50d946167b1f4ad049766f664e1efab37c6d318
jawsbar <radio700@hanmail.net>
2023-08-06 21:39:40 +0900

    before_mui_injection

--- src/main/front/src/page/sys/Cars.js
+++ src/main/front/src/page/sys/Cars.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { SERVER_URL } from "../../constants";
+// import { DataGrid } from "@mui/x-data-grid";
 
 function Cars(){
 
@@ -16,20 +17,52 @@ function Cars(){
       .then(response => response.json())
       .then(json =>{
           setCars(json._embedded.cars);
-          console.log(json._embedded.cars);
-          //setMovies(json.data.movies);
-  })
-  },[])//[]안에 있는 useState는 수가 바뀔 때 마다 재 랜더링됨
+          console.log(json);
+      })
+      .catch((err) => console.error(err));
+  },[])
 
   /*
   * User Function
   */
-  
+  const onDelClick = (url) => {
+    if(window.confirm(` ${url} are you sure?`)){
+      fetch(url, {method: "DELETE"})
+      .then(response => fncallback())
+      .catch((err) => console.error(err));
+    }
+
+  }
+
+  const fncallback = () => {
+    fetch(url)
+      .then(response => response.json())
+      .then(json => setCars(json._embedded.cars))
+      .catch((err) => console.error(err));
+      console.log("콜백됨");
+  }
+
+  const columns = [
+    {field: 'brand', headerName: 'brand',  width: 200},
+    {field: 'model', headerName: 'model',  width: 200},
+    {field: 'color', headerName: 'color',  width: 200},
+    {field: 'year', headerName: 'year',  width: 200},
+    {field: 'price', headerName: 'price',  width: 200},
+  ]
 
   /*
   * HTML {javascript}
   */
     return(
+      // <div style={{height:500, width:'100%'}}>
+      //   <DataGrid
+      //     rows={Cars}
+      //     columns={columns}
+      //     getRowId={row => row._links.self.href}
+      //   >
+
+      //   </DataGrid>
+      // </div>
       <div>
         <table>
           <thead>
@@ -47,7 +80,8 @@ function Cars(){
                   <td>{car.color}</td>
                   <td>{car.year}</td>
                   <td>{car.price}</td>
-                  <button >asdf</button>
+                  <td>{car.id}</td>
+                  <button onClick={() => onDelClick(car.id)}>delete</button>
                 </tr>
               )
             }
